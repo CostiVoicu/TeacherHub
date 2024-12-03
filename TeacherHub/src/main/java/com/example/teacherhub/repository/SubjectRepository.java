4e01f806bb4eea993b0c872766c4024236c6d018
Constantin Voicu <voicu.constantin@outlook.com>
2024-12-03 10:50:38 +0200

    Add subject repository method to get grades for subject

--- TeacherHub/src/main/java/com/example/teacherhub/repository/SubjectRepository.java
+++ TeacherHub/src/main/java/com/example/teacherhub/repository/SubjectRepository.java
@@ -1,9 +1,11 @@
 package com.example.teacherhub.repository;
 
+import com.example.teacherhub.model.Grade;
 import com.example.teacherhub.model.Subject;
 
 import java.util.List;
 
 public interface SubjectRepository {
     List<Subject> getSubjectsByStudentId(int studentId);
+    List<Grade> getGradesForSubject(int studentId, int subjectId);
 }
--- TeacherHub/src/main/java/com/example/teacherhub/repository/impl/SubjectRepositoryImpl.java
+++ TeacherHub/src/main/java/com/example/teacherhub/repository/impl/SubjectRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.example.teacherhub.repository.impl;
 
+import com.example.teacherhub.model.Grade;
 import com.example.teacherhub.model.Subject;
 import com.example.teacherhub.repository.SubjectRepository;
 import com.example.teacherhub.utils.DBConnectionUtil;
@@ -34,4 +35,35 @@ public class SubjectRepositoryImpl implements SubjectRepository {
         }
         return subjects;
     }
+
+    @Override
+    public List<Grade> getGradesForSubject(int studentId, int subjectId) {
+        List<Grade> grades = new ArrayList<>();
+        String sql = "{CALL GetGradesForSubject(?, ?)}";
+
+        try (Connection connection = DBConnectionUtil.getConnection();
+             PreparedStatement preparedStatement = connection.prepareCall(sql)) {
+
+            preparedStatement.setInt(1, studentId);
+            preparedStatement.setInt(2, subjectId);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    Grade grade = new Grade();
+                    grade.setGradeID(resultSet.getInt("GradeID"));
+                    grade.setGrade(resultSet.getDouble("Grade"));
+                    grade.setStudentID(resultSet.getInt("StudentID"));
+                    grade.setSubjectID(resultSet.getInt("SubjectID"));
+                    grade.setDateAssigned(resultSet.getDate("DateAssigned"));
+                    grades.add(grade);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return grades;
+    }
+
+
 }
